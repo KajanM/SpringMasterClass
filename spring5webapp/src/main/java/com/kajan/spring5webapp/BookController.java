f27b624a73b70c7062780ed901ec0b5bab739a7a
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-21 09:13:24 +0530

    added BookController to get all books from DB

---
+++ spring5webapp/src/main/java/com/kajan/spring5webapp/BookController.java
@@ -0,0 +1,22 @@
+package com.kajan.spring5webapp;
+
+import com.kajan.spring5webapp.repository.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
