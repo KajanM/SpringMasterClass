1d7f3241d39b0bd5ba16943e870202e781e0dc69
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-21 09:48:17 +0530

    added thymeleaf template to show books list

--- spring5webapp/src/main/java/com/kajan/spring5webapp/model/Author.java
+++ spring5webapp/src/main/java/com/kajan/spring5webapp/model/Author.java
@@ -30,6 +30,14 @@ public class Author {
         this.books = books;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
