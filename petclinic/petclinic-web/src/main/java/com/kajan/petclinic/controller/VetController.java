f135bf6b578e4d0c9312c2fa78814be47ebe09a4
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-22 18:47:36 +0530

    vet index page and controller. closes #7

---
+++ petclinic/petclinic-web/src/main/java/com/kajan/petclinic/controller/VetController.java
@@ -0,0 +1,14 @@
+package com.kajan.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetController {
+
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+    }
+}
