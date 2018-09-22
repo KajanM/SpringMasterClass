4d73d34386bf97533c37790713ed9068772a6933
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-22 16:02:49 +0530

    added JokeController

---
+++ jokeapp/src/main/java/com/kajan/jokeapp/controller/JokeController.java
@@ -0,0 +1,25 @@
+package com.kajan.jokeapp.controller;
+
+import com.kajan.jokeapp.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
