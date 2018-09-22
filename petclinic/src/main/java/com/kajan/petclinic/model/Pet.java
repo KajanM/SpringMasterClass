d7111440142041f5d3de7889c007cc8f2ee458f1
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-22 11:40:26 +0530

    added POJOs closes #2

---
+++ petclinic/src/main/java/com/kajan/petclinic/model/Pet.java
@@ -0,0 +1,10 @@
+package com.kajan.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+}
---
+++ petclinic/src/main/java/com/kajan/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.kajan.petclinic.model;
+
+public class Vet extends Person {
+}
