f6b72351e761a8dd55a629779e27215a4db60a1a
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-22 18:30:10 +0530

    pet clinic index page and controller. closes #8

---
+++ petclinic/petclinic-web/src/main/java/com/kajan/petclinic/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.kajan.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
