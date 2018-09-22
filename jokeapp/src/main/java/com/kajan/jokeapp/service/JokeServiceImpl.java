53d49659b114bb84be315344d84be1447a1395a7
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-22 19:30:14 +0530

    configure ChuckNorrisQuotes as a spring bean

--- jokeapp/src/main/java/com/kajan/jokeapp/service/JokeServiceImpl.java
+++ jokeapp/src/main/java/com/kajan/jokeapp/service/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kajan.jokeapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,8 +9,9 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    @Autowired
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
