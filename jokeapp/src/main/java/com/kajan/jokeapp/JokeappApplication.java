f20caf6f2b217d1a556f7511c4f58524283bb45f
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-22 15:30:02 +0530

    initialized spring boot joke app

---
+++ jokeapp/src/main/java/com/kajan/jokeapp/JokeappApplication.java
@@ -0,0 +1,12 @@
+package com.kajan.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+    }
+}
