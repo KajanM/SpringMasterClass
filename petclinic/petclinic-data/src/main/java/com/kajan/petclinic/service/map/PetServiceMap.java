290f01e27f26b18dc4ddaf3d00e2a90c549e5635
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-22 18:07:36 +0530

    impl map based services. closes #6

---
+++ petclinic/petclinic-data/src/main/java/com/kajan/petclinic/service/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.kajan.petclinic.service.map;
+
+import com.kajan.petclinic.model.Pet;
+import com.kajan.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+}
---
+++ petclinic/petclinic-data/src/main/java/com/kajan/petclinic/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.kajan.petclinic.service.map;
+
+import com.kajan.petclinic.model.Vet;
+import com.kajan.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+}
