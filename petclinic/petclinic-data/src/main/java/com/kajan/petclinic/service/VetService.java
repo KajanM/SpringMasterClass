2bd242c61fd48cfba09d5f39d6e5d69f1988e527
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-09-22 15:09:23 +0530

    create owner, pet, vet service interfaces. Closes #5

---
+++ petclinic/petclinic-data/src/main/java/com/kajan/petclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package com.kajan.petclinic.service;
+
+import com.kajan.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
